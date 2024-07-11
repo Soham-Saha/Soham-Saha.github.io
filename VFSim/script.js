998ae28fc10d395ed8d8ab912187ebef4c9abc93
Soham-Saha <soham123gogol@gmail.com>
2024-07-11 20:50:54 +0530

    Modified

--- VFSim/script.js
+++ VFSim/script.js
@@ -20,41 +20,56 @@ canv.width = Math.min(window.innerWidth, window.innerHeight) / 1.1;
 canv.height = canv.width;
 mem = []
 
-function randPoint() {
-        return new Point(Math.random() * canv.width, Math.random() * canv.height);
-}
-
-s = []
-for (i = 0; i < PT_COUNT; i++) {
-        s.push(randPoint());
+s = new Float32Array(PT_COUNT*2)
+for (i = 0; i < PT_COUNT*2; i+=2) {
+        s[i]=Math.random() * canv.width;
+        s[i+1]=Math.random() * canv.height;
 }
 function viewUpdater() {
         ctx.fillStyle = 'black'
         ctx.fillRect(0, 0, canv.width, canv.height)
-        const snap = s.map(p => [p.x, p.y]);
-        mem.push(snap);
+        mem.push(s.map(k=>k));
         if (mem.length > MEM_LENGTH) {
                 mem.shift();
         }
         mem_i = 0
-        mem.forEach(memInst => {
+        for(memCt=0;memCt<mem.length;memCt++){
                 c = [0, 255, 255]
-                ctx.fillStyle = `rgba(${c[0] * mem_i / MEM_LENGTH},${c[1] * mem_i / MEM_LENGTH},${c[2] * mem_i / MEM_LENGTH})`
-                memInst.forEach(p => {
-                        ctx.fillRect(p[0], p[1], 0.5+1*mem_i/MEM_LENGTH, 0.5+1*mem_i/MEM_LENGTH)
-                });
-                mem_i++
-        });
+                ctx.fillStyle = `rgba(${c[0] * memCt/ MEM_LENGTH},${c[1] * memCt / MEM_LENGTH},${c[2] * memCt / MEM_LENGTH})`
+                for(i=0;i<PT_COUNT*2;i+=2){
+                        ctx.fillRect(mem[memCt][i],mem[memCt][i+1],0.5+1*memCt/MEM_LENGTH, 0.5+1*memCt/MEM_LENGTH);
+                }
+        }  
+        
+        
+        //NEED TO REWRITE
+        /*-ctx.fillStyle = 'black'
+        ctx.fillRect(0, 0, canv.width, canv.height)
+        for(i=0;i<PT_COUNT*2;i+=2){
+                mem[memIndex*PT_COUNT*2+i]=s[i]
+                mem[memIndex*PT_COUNT*2+i+1]=s[i+1]
+        }
+        memIndex=(memIndex+1)%MEM_LENGTH
+        mem_i = 0
+        for(memCt1=0;memCt1<MEM_LENGTH;memCt1++){
+                memCt=(memIndex+memCt1)%MEM_LENGTH
+                c = [0, 255, 255]
+                ctx.fillStyle = `rgba(${c[0] * memCt/ MEM_LENGTH},${c[1] * memCt / MEM_LENGTH},${c[2] * memCt / MEM_LENGTH})`
+                for(i=0;i<PT_COUNT*2;i+=2){
+                        ctx.fillRect(mem[memCt*PT_COUNT*2+i],mem[memCt*PT_COUNT*2+i+1],0.5+1*memCt/MEM_LENGTH, 0.5+1*memCt/MEM_LENGTH);
+                }
+        }*/ 
+        
 }
 function pointUpdater() {
-        for (i = 0; i < s.length; i++) {
-                p = s[i]
-                p.vx = Math.log(Math.abs((p.y - canv.height / 2) / 50));
-                p.vy = Math.log(Math.abs((p.x - canv.width / 2) / 50));
-                p.x = p.x + p.vx * 0.5;
-                p.y = p.y + p.vy * 0.5;
-                if (p.x > canv.width || p.x < 0 || p.y > canv.height || p.y < 0 || Math.random() < 0.01) {
-                        s[i] = randPoint()
+        for (i = 0; i < s.length; i+=2) {
+                vx = Math.log(Math.abs((s[i+1] - canv.height / 2) / 50));
+                vy = Math.log(Math.abs((s[i] - canv.width / 2) / 50));
+                s[i] = s[i] + vx * 0.5;
+                s[i+1] = s[i+1] + vy * 0.5;
+                if (s[i] > canv.width || s[i] < 0 || s[i+1] > canv.height || s[i+1] < 0 || Math.random() < 0.01) {
+                        s[i] = Math.random() *canv.width;
+                        s[i+1] = Math.random() *canv.height;
                 }
         }
 }
